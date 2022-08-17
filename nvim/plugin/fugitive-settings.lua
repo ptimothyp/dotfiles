730a5a995de284b699940f7f06cf4d1ddb4ea859
Tim Pakkianathan <ptimothyp@gmail.com>
2022-08-17 18:38:52 +1000

    Fugitive settings.

---
+++ nvim/plugin/fugitive-settings.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('n', '<leader>gs', ':Git<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>gps', ':Git push<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>gpl', ':Git pull<CR>', {noremap = true})
