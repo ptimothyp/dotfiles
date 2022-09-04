0cb89d16f0dc29db27f5fec63bef5947cdf0b39b
Tim <ptimothyp@gmail.com>
2022-09-04 10:54:43 +1000

    Including nvm-comp, switching from commentary.

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -1,65 +1,81 @@
 return require('packer').startup(function()
 
-	--packer can manage itself
-	use 'wbthomason/packer.nvim'
+  --packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use 'christoomey/vim-sort-motion'
-	use 'christoomey/vim-system-copy'
-	use 'christoomey/vim-titlecase'
-	use 'easymotion/vim-easymotion'
-	use 'editorconfig/editorconfig-vim'
-	use 'gruvbox-community/gruvbox'
-	use 'honza/vim-snippets'
-	use 'inkarkat/vim-replacewithregister'
-	use 'inkarkat/vim-visualrepeat'
-	use 'ionide/Ionide-vim'
-	use 'janko-m/vim-test'
-	use 'jparise/vim-graphql'
-	use 'jremmen/vim-ripgrep'
-	use 'ryanoasis/vim-devicons'
-	-- use 'preservim/nerdtree'
-	use 'scrooloose/nerdtree'
+  use 'christoomey/vim-sort-motion'
+  use 'christoomey/vim-system-copy'
+  use 'christoomey/vim-titlecase'
+  use 'easymotion/vim-easymotion'
+  use 'editorconfig/editorconfig-vim'
+  use 'gruvbox-community/gruvbox'
+  use 'honza/vim-snippets'
+  use 'inkarkat/vim-replacewithregister'
+  use 'inkarkat/vim-visualrepeat'
+  use 'ionide/Ionide-vim'
+  use 'janko-m/vim-test'
+  use 'jparise/vim-graphql'
+  use 'jremmen/vim-ripgrep'
+  use 'ryanoasis/vim-devicons'
+  -- use 'preservim/nerdtree'
+  use 'scrooloose/nerdtree'
   use({
       "junegunn/fzf",
     run = function() vim.fn["fzf#install()"]() end,
   })
-	use 'junegunn/fzf.vim'
-	use 'kana/vim-textobj-entire'
-	use 'kana/vim-textobj-indent'
-	use 'kana/vim-textobj-user'
-	use 'kassio/neoterm'
-	use 'kevinoid/vim-jsonc'
-	use 'leafgarland/typescript-vim'
-	use 'machakann/vim-highlightedyank'
-	use 'mattn/emmet-vim'
-	use 'mhinz/vim-grepper'
-	use 'mhinz/vim-signify'
- use 'neovim/nvim-lspconfig'
-	-- use 'neoclide/coc.nvim'
-	-- use {'neoclide/coc.nvim', branch = 'release'}
-	use 'neovimhaskell/haskell-vim'
-	use 'omniSharp/omniSharp-vim'
-	use 'pangloss/vim-javascript'
-	use 'peitalin/vim-jsx-typescript'
-	use 'pprovost/vim-ps1'
-	use 'rust-lang/rust.vim'
-	use 'shaunsingh/moonlight.nvim'
-	use 'stefandtw/quickfix-reflector.vim'
-	use 'tpope/vim-commentary'
-	use 'tpope/vim-fugitive'
-	use 'tpope/vim-repeat'
-	use 'tpope/vim-surround'
-	use 'tpope/vim-unimpaired'
-	use 'vim-syntastic/syntastic'
-	use({
-			"iamcco/markdown-preview.nvim",
-			run = function() vim.fn["mkdp#util#install"]() end,
-	})
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	-- or                            , branch = '0.1.x',
-			requires = { {'nvim-lua/plenary.nvim'} }
+  use 'junegunn/fzf.vim'
+  use 'kana/vim-textobj-entire'
+  use 'kana/vim-textobj-indent'
+  use 'kana/vim-textobj-user'
+  use 'kassio/neoterm'
+  use 'kevinoid/vim-jsonc'
+  use 'leafgarland/typescript-vim'
+  use 'machakann/vim-highlightedyank'
+  use 'mattn/emmet-vim'
+  use 'mhinz/vim-grepper'
+  use 'mhinz/vim-signify'
+  use 'neovim/nvim-lspconfig'
+  -- use 'neoclide/coc.nvim'
+  -- use {'neoclide/coc.nvim', branch = 'release'}
+  use 'neovimhaskell/haskell-vim'
+  use 'omniSharp/omniSharp-vim'
+  use 'pangloss/vim-javascript'
+  use 'peitalin/vim-jsx-typescript'
+  use 'pprovost/vim-ps1'
+  use 'rust-lang/rust.vim'
+  use 'shaunsingh/moonlight.nvim'
+  use 'stefandtw/quickfix-reflector.vim'
+  -- use 'tpope/vim-commentary'
+  use 'tpope/vim-fugitive'
+  use 'tpope/vim-repeat'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-unimpaired'
+  use 'vim-syntastic/syntastic'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
 
+  use 'hrsh7th/cmp-vsnip'
+  use 'hrsh7th/vim-vsnip'
+
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+  -- or                            , branch = '0.1.x',
+      requires = { {'nvim-lua/plenary.nvim'} }
+
+  }
+
+  use {
+      'numToStr/Comment.nvim',
+      config = function()
+          require('Comment').setup()
+      end
   }
   use 'nvim-treesitter/nvim-treesitter'
 end)
