c3fe3a39e3c32437151a3a02d6f13f51ef24ce86
Tim <ptimothyp@gmail.com>
2022-09-10 04:30:27 +1000

    Cleanup.

--- nvim/plugin/nvim-lsp-settings.lua
+++ nvim/plugin/nvim-lsp-settings.lua
@@ -36,15 +36,15 @@ local lsp_flags = {
   -- This is the default in Nvim 0.7+
   debounce_text_changes = 150,
 }
-require('lspconfig')['pyright'].setup{
+require'lspconfig'.pyright.setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
-require('lspconfig')['tsserver'].setup{
+require'lspconfig'.tsserver.setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
-require('lspconfig')['hls'].setup{
+require'lspconfig'.hls.setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
@@ -60,7 +60,7 @@ require'lspconfig'.powershell_es.setup{
   shell = pwShell,
 }
 
-require('lspconfig')['sumneko_lua'].setup{
+require'lspconfig'.sumneko_lua.setup{
     on_attach = on_attach,
     flags = lsp_flags,
     settings = {
