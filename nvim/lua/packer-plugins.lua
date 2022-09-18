320f39bed590ca6a6b6daa85339b4b700a168aa9
Timothy <ptimothyp@gmail.com>
2022-09-19 09:08:09 +1000

    Auto install packer and sync.

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function()
 
   --packer can manage itself
@@ -95,5 +108,9 @@ use {
     'ThePrimeagen/harpoon',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+ if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
 
