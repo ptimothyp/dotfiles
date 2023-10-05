f05b53c2b1e7264e1508d0266291bd549970bde7
Tim <ptimothyp@gmail.com>
2023-10-05 20:43:57 +1100

    Tab to space.

--- nvim/plugin/nvim-lsp-settings.lua
+++ nvim/plugin/nvim-lsp-settings.lua
@@ -10,77 +10,77 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = vim.api.nvim_create_augroup("Format", { clear = true }),
-			buffer = bufnr,
-			callback = function() vim.lsp.buf.formatting_seq_sync() end
-		})
-	end
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.formatting_seq_sync() end
+    })
+  end
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-	-- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-	vim.keymap.set('n', '<space>wl', function()
-		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	end, bufopts)
-	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-	vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<space>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.format, bufopts)
 end
 
 local lsp_flags = {
-	-- This is the default in Nvim 0.7+
-	debounce_text_changes = 150,
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
 }
 require 'lspconfig'.pyright.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
 require 'lspconfig'.tsserver.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
 require 'lspconfig'.hls.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
+  on_attach = on_attach,
+  flags = lsp_flags,
 }
 
 local pwBundlePath = 'C:/tools/PowerShellEditorServices'
 local pwShell = 'powershell.exe'
 if vim.fn.has('macunix') ~= 0 then
-	pwBundlePath = '/Users/timothy/PowerShellEditorServices'
-	pwShell = 'pwsh'
+  pwBundlePath = '/Users/timothy/PowerShellEditorServices'
+  pwShell = 'pwsh'
 end
 
 require 'lspconfig'.powershell_es.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	bundle_path = pwBundlePath,
-	shell = pwShell
+  on_attach = on_attach,
+  flags = lsp_flags,
+  bundle_path = pwBundlePath,
+  shell = pwShell
 }
 
 require 'lspconfig'.lua_ls.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim", "use", "it", "describe" }
-			}
-		}
-	}
+  on_attach = on_attach,
+  flags = lsp_flags,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { "vim", "use", "it", "describe" }
+      }
+    }
+  }
 }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
@@ -88,10 +88,10 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 local omnisharp_bin = "/Users/timothy/binaries/omnisharp-osx-arm64-net6/omnisharp"
 local pid = vim.fn.getpid()
 require 'lspconfig'.omnisharp.setup {
-	on_attach = on_attach,
-	flags = lsp_flags,
-	capabilities = capabilities,
-	cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) }
-	-- Additional configuration can be added here
+  on_attach = on_attach,
+  flags = lsp_flags,
+  capabilities = capabilities,
+  cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) }
+  -- Additional configuration can be added here
 }
 -- require'lspconfig'.csharp_ls.setup{}
