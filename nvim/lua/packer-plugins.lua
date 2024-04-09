7100fb2668068a920969757ce5c3e66219ae9612
Tim <ptimothyp@gmail.com>
2024-04-09 20:40:24 +1000

    Added codium

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -140,6 +140,19 @@ return require('packer').startup(function()
       -- see below for full list of optional dependencies 👇
     },
   })
+  use({
+    'Exafunction/codeium.vim',
+    config = function()
+      vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+        { expr = true, silent = true })
+      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  })
+
+
   if packer_bootstrap then
     require('packer').sync()
   end
