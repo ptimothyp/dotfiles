522f8bc044a32ea71ac38b9cdf837a296e5862d3
Timothy Pakkianathan <v-timothyp@microsoft.com>
2024-04-13 20:24:24 +1000

    Added more harpoon keymaps.

--- nvim/plugin/harpoon-settings.lua
+++ nvim/plugin/harpoon-settings.lua
@@ -10,7 +10,7 @@ local silent = { silent = true }
 vim.keymap.set('n', "<leader>a", function() require("harpoon.mark").add_file() end, silent)
 vim.keymap.set('n', "<C-e>", function() require("harpoon.ui").toggle_quick_menu() end, silent)
 
--- vim.keymap.set('n', "<C-1>", function() require("harpoon.ui").nav_file(1) end, silent)
--- vim.keymap.set('n', "<C-2>", function() require("harpoon.ui").nav_file(2) end, silent)
--- vim.keymap.set('n', "<C-3>", function() require("harpoon.ui").nav_file(3) end, silent)
--- vim.keymap.set('n', "<C-4>", function() require("harpoon.ui").nav_file(4) end, silent)
+vim.keymap.set('n', "<leader>f1", function() require("harpoon.ui").nav_file(1) end, silent)
+vim.keymap.set('n', "<leader>f2", function() require("harpoon.ui").nav_file(2) end, silent)
+vim.keymap.set('n', "<leader>f3", function() require("harpoon.ui").nav_file(3) end, silent)
+vim.keymap.set('n', "<leader>f4", function() require("harpoon.ui").nav_file(4) end, silent)
