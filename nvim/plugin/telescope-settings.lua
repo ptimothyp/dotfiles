fdb1d6063054a0a645f8e82ce701c569d4795194
Tim <ptimothyp@gmail.com>
2024-05-04 19:24:57 +1000

    Ignore git files

--- nvim/plugin/telescope-settings.lua
+++ nvim/plugin/telescope-settings.lua
@@ -1,4 +1,11 @@
 local tBuiltIn = require 'telescope.builtin'
+require('telescope').setup {
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules"
+    }
+  }
+}
 vim.keymap.set('n', '<space>ff', tBuiltIn.find_files, {})
 vim.keymap.set('n', '<space>fg', tBuiltIn.grep_string, {})
 vim.keymap.set('n', '<space>fb', tBuiltIn.buffers, {})
