66ca48b8b3f7260ff081d9990556dd23c0a79665
Tim <ptimothyp@gmail.com>
2024-05-14 09:06:14 +1000

    Cleanup

--- nvim/lua/keyboard-shortcuts.lua
+++ nvim/lua/keyboard-shortcuts.lua
@@ -1,16 +1,24 @@
 local nmap = function(name, map)
   vim.keymap.set('n', name, map)
 end
+
+-- Navigate between windows when in normal mode
 nmap('<C-J>', '<C-W><C-J>')
 nmap('<C-H>', '<C-W><C-H>')
 nmap('<C-K>', '<C-W><C-K>')
 nmap('<C-L>', '<C-W><C-L>')
 nmap('<leader>zi', ':e $MYVIMRC<CR>')
 
+-- Navigate between windows when in terminal mode
 nmap('<M-.>', '<C-W>5<')
 nmap('<M-,>', '<C-W>5>')
 nmap('<M-t>', '<C-W>+')
 nmap('<M-s>', '<C-W>-')
+
+vim.keymap.set('t', '<C-J>', '<C-\\><C-n><C-W><C-J>')
+vim.keymap.set('t', '<C-H>', '<C-\\><C-n><C-W><C-H>')
+vim.keymap.set('t', '<C-K>', '<C-\\><C-n><C-W><C-K>')
+vim.keymap.set('t', '<C-L>', '<C-\\><C-n><C-W><C-L>')
 
 -- vim.keymap.set('n', '<leader><CR>', ':so $MYVIMRC<CR>')
 nmap('<leader><CR>', ':so %<CR>')
@@ -18,10 +26,6 @@ nmap('<leader>w', ':up<CR>')
 nmap('<leader><space>', ':FZF<CR>')
 nmap('<leader>b', ':Buffers<CR>')
 
-vim.keymap.set('t', '<C-J>', '<C-\\><C-n><C-W><C-J>')
-vim.keymap.set('t', '<C-H>', '<C-\\><C-n><C-W><C-H>')
-vim.keymap.set('t', '<C-K>', '<C-\\><C-n><C-W><C-K>')
-vim.keymap.set('t', '<C-L>', '<C-\\><C-n><C-W><C-L>')
 
 local stackmap = require("stackmap")
 stackmap.push("lua_test", "n", {
