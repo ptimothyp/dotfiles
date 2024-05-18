5fe1db25b18001c9d8fc7d79d4d4992724eabf49
Tim <ptimothyp@gmail.com>
2024-05-19 04:56:34 +1000

    Oil file manager

--- nvim/lua/packer-plugins.lua
+++ nvim/lua/packer-plugins.lua
@@ -192,6 +192,32 @@ return require('packer').startup(function()
 
   use { 'nvim-telescope/telescope-ui-select.nvim' }
 
+
+  use {
+    'stevearc/oil.nvim',
+
+    requires = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("oil").setup {
+        columns = { "icon" },
+        keymaps = {
+          ["<C-h>"] = false,
+          ["<M-h>"] = "actions.select_split",
+        },
+        view_options = {
+          show_hidden = true,
+        },
+      }
+      -- Open parent directory in current window
+      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Open parent directory in floating window
+      vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+    end,
+  }
+
   if packer_bootstrap then
     require('packer').sync()
   end
