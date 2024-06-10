2d245115b4b1646f843b50f26e3c261389ac41b6
Timothy Pakkianathan <ptimothyp@gmail.com>
2024-06-10 23:21:44 +1000

    Mason updates and grep config

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,4 +1,13 @@
 return {
+  {
     'williamboman/mason.nvim',
+  },
+  {
     'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup{
+        ensure_installed = {"lua_ls"}
+      }
+    end
+  }
 }
