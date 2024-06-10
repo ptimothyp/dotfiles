60a308e93b189dd8e62cf5f1bf45dc754d9003b8
Timothy Pakkianathan <ptimothyp@gmail.com>
2024-06-11 00:45:43 +1000

    Fixed copy and the grepper

--- nvim/lua/plugins/christoomey-standard.lua
+++ nvim/lua/plugins/christoomey-standard.lua
@@ -1,3 +1,13 @@
 return {
-  'christoomey/vim-system-copy'
+  'christoomey/vim-system-copy',
+  config = function()
+    vim.cmd(
+      [[
+if has('Win64')
+  let g:system_copy#copy_command='clip'
+  let g:system_copy#paste_command='powershell.exe -NoLogo -NoProfile -Noninteractive -Command "Get-Clipboard"'
+endif
+      ]]
+    )
+  end
 }
--- nvim/lua/plugins/vim-grepper.lua
+++ nvim/lua/plugins/vim-grepper.lua
@@ -3,10 +3,13 @@ return
   {
     'mhinz/vim-grepper',
     config = function()
-      -- vim.g.grepper = {}
-      -- vim.g.grepper.tools = { 'rg', 'grep', 'git' }
-      vim.keymap.set('n', '<leader>G', ':Grepper --tool rg<cr>', opts)
-      vim.keymap.set('n', '<leader>g', ':Grepper --tool git<cr>', opts)
+      vim.cmd([[
+let g:grepper = {}
+let g:grepper.tools = ['rg', 'grep', 'git']
+          ]])
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '<leader>G', ':Grepper -tool rg<cr>', opts)
+      vim.keymap.set('n', '<leader>g', ':Grepper -tool git<cr>', opts)
     end
   }
 }
